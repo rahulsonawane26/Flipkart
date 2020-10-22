ee7b2255f5f22a90821888eb0e29916ed47c7f16
rahul <rahul@LAPTOP-LFPBBS7T>
2020-10-23 02:37:37 +0530

    Name and Price of Iphone in HashMap

--- src/test/java/PageObject/SearchResult.java
+++ src/test/java/PageObject/SearchResult.java
@@ -1,5 +1,7 @@
 package PageObject;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -14,32 +16,43 @@ import TestCases.CommonMethods;
 public class SearchResult {
 
 	WebDriver driver;
-	
-	
-	public SearchResult(){
-		
-		driver= CommonMethods.driver;
+
+	public SearchResult() {
+
+		driver = CommonMethods.driver;
 	}
-	
+
 	public void price(String conditon) throws InterruptedException {
-		
+
 		Thread.sleep(5000);
-		Select pricedd = new Select(driver.findElement(By.xpath("(//*[@class='fPjUPw'])[2]"))); 
+		Select pricedd = new Select(driver.findElement(By.xpath("(//*[@class='fPjUPw'])[2]")));
 		pricedd.selectByIndex(9);
 		Thread.sleep(5000);
 		WebElement firstproductname = driver.findElement(By.xpath("(//div[@class='_3wU53n'])[1]"));
 		firstproductname.click();
-		
+
 	}
-	
+
 	public void searchresultn() throws InterruptedException {
-		
+
 		Thread.sleep(5000);
 		List<WebElement> one = driver.findElements(By.xpath("(//div[@class='_3O0U0u'])[1]"));
 		List<WebElement> two = driver.findElements(By.xpath("(//div[@class='_3O0U0u'])[2]"));
 		one.addAll(two);
 		System.out.println(one);
-		
+
 	}
-	
+
+	public List phonename() {
+
+		List<WebElement> name = driver.findElements(By.xpath("//div[@class='_3wU53n']"));
+		return name;
+	}
+
+	public List phoneprice() {
+
+		List<WebElement> price = driver.findElements(By.xpath("//div[@class='_1vC4OE _2rQ-NK']"));
+		return price;
+
+	}
 }
